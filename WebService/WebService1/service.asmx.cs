61e9352f0264dd0124bde890aafa3f7736ec7919
kratin <mintra081@gmail.com>
2012-05-16 23:55:38 +0700

    Commit #1 WebService by Mintra
    Signed-off-by: kratin <mintra081@gmail.com>

---
+++ WebService/WebService1/service.asmx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.Xml.Linq;
+
+namespace WebService
+{
+    /// <summary>
+    /// Summary description for service
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class service : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public string HelloWorld2(String name)
+        {
+            return "Hello "+name;
+        }
+    }
+}
---
+++ WebSite/Default.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Untitled Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    Enter Your Name :<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br /><br />
+    <asp:Button ID="Button1" runat="server" Text="Submit" onclick="Button1_Click" /><br /><br />
+    <asp:Label ID="Label1" runat="server" Text="Result..."></asp:Label>
+    <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+    </form>
+</body>
+</html>
